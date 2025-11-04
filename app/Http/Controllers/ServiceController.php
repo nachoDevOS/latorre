284255aa83d97ad8a5c034265c2f4284ef50e373
nachoDevOs <ignaciomolinaguzman20@gmail.com>
2025-11-04 15:37:35 -0400

    dev

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Room;
 
 class ServiceController extends Controller
 {
@@ -14,6 +15,7 @@ class ServiceController extends Controller
 
     public function index()
     {
-        return view('services.index');
+        $rooms = Room::where('deleted_at', NULL)->get();
+        return view('services.index', compact('rooms'));
     }
 }
--- resources/views/Services/index.blade.php
+++ resources/views/Services/index.blade.php
@@ -1,30 +1,5 @@
 @extends('voyager::master')
 
-@section('page_title', 'Viendo Servicios')
-
-@section('page_header')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="panel panel-bordered">
-                    <div class="panel-body" style="padding: 0px">
-                        <div class="col-md-8" style="padding: 0px">
-                            <h1 class="page-title">
-                                <i class="fa-solid fa-gamepad"></i> Salas de Juegos
-                            </h1>
-                        </div>
-                        <div class="col-md-4 text-right" style="margin-top: 30px">
-                            <a href="{{ route('voyager.rooms.index') }}" class="btn btn-primary">
-                                <i class="fa-solid fa-eye"></i> <span>Ver Salas</span>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-@stop
-
 @section('content')
     <div class="page-content browse container-fluid">
         @include('voyager::alerts')
@@ -35,6 +10,44 @@
                         <div class="alert alert-info">
                             <h4 class="text-center">Bienvenido al módulo de servicios</h4>
                             <p class="text-center">Desde aquí podrás gestionar las salas de juegos y otros servicios.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-bordered">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Salas de juegos</h3>
+                    </div>
+                    <div class="panel-body">
+                        <div class="row">
+                            @forelse ($rooms as $room)
+                                <div class="col-md-4">
+                                    <div class="panel panel-default">
+                                        <div class="panel-heading">
+                                            <h3 class="panel-title">{{ $room->name }}</h3>
+                                        </div>
+                                        <div class="panel-body">
+                                            <p>Tipo: {{ $room->type }}</p>
+                                            <p>Estado: 
+                                                @if ($room->status)
+                                                    <label class="label label-success">Disponible</label>
+                                                @else
+                                                    <label class="label label-danger">Ocupada</label>
+                                                @endif
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            @empty
+                                <div class="col-md-12">
+                                    <div class="alert alert-warning">
+                                        <h4 class="text-center">No hay salas de juegos registradas</h4>
+                                    </div>
+                                </div>
+                            @endforelse
                         </div>
                     </div>
                 </div>
